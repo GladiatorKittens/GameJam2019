30a3cef79f401ed02c7ac1f2487494d433f3fe83
GladiatorKittens <43640306+GladiatorKittens@users.noreply.github.com>
2019-01-26 00:15:10 +0000

    End Of Day One
    Got Lots done

--- js/create.js
+++ js/create.js
@@ -1,18 +1,22 @@
 function createTilemap(mapKey) {
     var map = this.make.tilemap({ key: mapKey });
-    var tileSet = map.addTilesetImage("TILESETPLACEHOLDER", "PLACEHOLDER2");
+    var tileSet = map.addTilesetImage("landscape", "tilesheet");
     //create layers
-    map.createStaticLayer("bg2", tileSet, 0, 0);
-    map.createStaticLayer("bg1", tileSet, 0, 0);
-    map.createStaticLayer("para", tileSet, 0, 0);
-    map.createStaticLayer("col", tileSet, 0, 0);
+    //map.createStaticLayer("sky", tileSet, 0, 0);
+    map.createStaticLayer("background", tileSet, 0, 0);
+    //map.createStaticLayer("para", tileSet, 0, 0);
+    map.createStaticLayer("collision", tileSet, 0, 0);
 
     //find player spawn point and create them
-    var playerSpawn = map.findObject("obj", function (object) {
-        if (object.type === "player") {
-            return object;
-        }
-    });
+    //var playerSpawn = map.findObject("obj", function (object) {
+    //    if (object.type === "player") {
+    //        return object;
+    //    }
+    //});
+    var playerSpawn = {
+        x: 10,
+        y: 10
+    }
     //player setup
     createPlayer.call(this, playerSpawn);
     //create any groups
@@ -22,23 +26,23 @@ function createTilemap(mapKey) {
     //createEnemies.call(this);
 
     //create foreground that displays on top
-    map.createStaticLayer("fg1", tileSet, 0, 0);
-    map.createStaticLayer("fg2", tileSet, 0, 0);
+   // map.createStaticLayer("fg1", tileSet, 0, 0);
+   // map.createStaticLayer("fg2", tileSet, 0, 0);
     return map;
 }
 function createPlayer(playerSpawn) {
     //TODO - edit in frame number
     player = new Player(this, playerSpawn.x, playerSpawn.y, "player", "frame_num");
-    player.setCollideWorldBounds(true);
+    player.sprite.setCollideWorldBounds(true);
 }
 function createCamera(map) {
-    var camera = this.camers.getCamera("");
+    var camera = this.cameras.getCamera("");
     camera.startFollow(player);
     camera.setBounds(0, 0, map.width * map.tileWidth, map.height * map.tileHeight);
     camera.zoom = 2;
 }
 function createCollision(map){
-    var collisionLayer = map.getLayer("col").tilemapLayer;
+    var collisionLayer = map.getLayer("collision").tilemapLayer;
     collisionLayer.setCollisionBetween(0, 1000);
     //TODO - here is where you add colliders
 }
--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 800,
+    width: 640,
+    height: 640,
     physics: {
         default: "arcade",
         arcade: {
@@ -13,7 +13,7 @@ var config = {
             }
         }
     },
-    pxelArt: true,
+    pixelArt: true,
     scene: {
         preload: preload,
         create: create,
@@ -23,15 +23,23 @@ var config = {
 //Any global variable declarations go here
 var game = new Phaser.Game(config);
 var player, home;
-var levelNum = 1;
+var levelNum = "1";
 function preload() {
     console.log(this);
-    //load in any assets here
-    //this.load.blank()
+    this.load.image("tilesheet", "/assets/tilesheet.png");
+    //this.load.image("")
+    //ALL TILEMAPS GO HERE
+    this.load.tilemapTiledJSON("1", "/assets/1.json");
+    this.load.spritesheet(
+        "player",
+        "/assets/CatAnims.png",
+        {frameWidth: 64, frameHeight: 64}
+    );
+    this.load.image("sword", "/assets/tempSword.png")
 };
 
 function create(){
-    var map = createTilemap.call(this, 1);//TODO, add in first map key
+    var map = createTilemap.call(this, levelNum);//TODO, add in first map key
     createCamera.call(this, map);
     createCollision.call(this, map);
     createKeys.call(this);
@@ -56,7 +64,7 @@ function endOfLevel(levelKey){
 function swordAnimation(){
     this.anims.create({
         key: "swipe",
-        frames: this.anims.generateFrameNumbers("sword", { frames: [] }),//TODO - implement frame numbers
+        frames: this.anims.generateFrameNumbers("sword", { start: 1, end: 3 }),//TODO - implement frame numbers
         frameRate: 5, //TODO - edit as neccessary for attack speed
         repeat: -1
     });
